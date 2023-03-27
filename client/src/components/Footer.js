559cd3fb4be07312acaac3250ce592afeab595dd
suhas1711 <shubham.bagwe@vit.edu.in>
2023-03-27 23:58:06 +0530

    Initial commit

---
+++ client/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import GitHubButton from 'react-github-btn';
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <div className="text">
+        <a href="/">
+        Made with{' '}
+        <span role="img" aria-label="f" aria-labelledby="f">
+          💖
+        </span></a>
+      </div>
+
+      
+    </div>
+  );
+};
+
+export default Footer;
